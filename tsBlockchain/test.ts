eeebd1972f230c3aee3f5de3d4444e3f9c0981df
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-19 17:49:18 -0600

    Add test.ts file with blockchain functionality.

---
+++ tsBlockchain/test.ts
@@ -0,0 +1,6 @@
+import Blockchain from './blockchain';
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
+bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
+bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
