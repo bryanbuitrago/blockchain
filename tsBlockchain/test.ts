2b81f3032febb8e0525c59ad888aff6983c6a9b1
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-23 14:56:19 -0600

    Add new block creation, transaction creation, and logging to test.ts.

--- tsBlockchain/test.ts
+++ tsBlockchain/test.ts
@@ -1,6 +1,31 @@
 import Blockchain from './blockchain';
+import { jsonStringify } from './utils';
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
-bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
-bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
+// ===<< Create New Block Tests >>===
+// bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
+// bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
+// bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
+// console.log(`[BlockChain] === ${jsonStringify(bitcoin)}`);
+// ===>> ::: <<===
+
+// ===<< Create New Transaction Tests >>===
+const newBlock = bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANDN0N90N');
+const pendingTransaction = bitcoin.createNewTransaction(
+  100,
+  'ALEX90NANAN',
+  'JEN90ANDN'
+);
+console.log(`[New Block] === ${jsonStringify(newBlock)}`);
+console.log(
+  `[Pending Transactions] === ${jsonStringify(bitcoin.pendingTransactions)}`
+);
+// ===>> ::: <<===
+
+// ===<< Get Last Block Test >>===
+console.log(`[Last Block] === ${jsonStringify(bitcoin.getLastBlock())}`);
+// ===>> ::: <<===
+
+// ===<< Log The BlockChain >>===
+console.log(`[BlockChain] === ${jsonStringify(bitcoin)}`);
+// ===>> ::: <<===
