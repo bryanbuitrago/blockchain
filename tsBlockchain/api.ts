0fcdfd757517152d35404d2fc4dc907a7cc2ce5a
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-04-02 15:29:17 -0400

    Configure express and test endpoint.

---
+++ tsBlockchain/api.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Blockchain API!!!!!!!!!!!!!!!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
