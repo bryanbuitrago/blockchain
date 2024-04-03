330f94038a3353a4d9d5994f337c148daa7729ce
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-04-02 23:06:22 -0400

    Add blockchain functionality to API.

--- tsBlockchain/api.ts
+++ tsBlockchain/api.ts
@@ -1,10 +1,27 @@
 import express from 'express';
-const app = express();
+import Blockchain from './blockchain.js';
 
-app.get('/', (req, res) => {
-  res.send('Blockchain API!!!!!!!!!!!!!!!');
+const app = express();
+const bitcoin = new Blockchain();
+
+console.log(bitcoin);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/blockchain', (req, res) => {
+  res.send(bitcoin);
+});
+
+app.post('/transaction', (req, res) => {
+  console.log(req.body);
+  res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
+});
+
+app.get('/mine', (req, res) => {
+  res.send('Mine a new block');
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Blockchain app listening on port 3000!');
 });
