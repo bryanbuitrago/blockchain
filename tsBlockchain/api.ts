4ff47a468596990d616aab640b65b73588d38846
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-04-03 00:14:56 -0400

    Add unique node address and update API endpoints.

--- tsBlockchain/api.ts
+++ tsBlockchain/api.ts
@@ -1,25 +1,62 @@
 import express from 'express';
 import Blockchain from './blockchain.js';
+import { randomUUID } from 'crypto';
 
+// Create a unique random node address for this instance of the blockchain
+const nodeAddress = randomUUID().split('-').join(''); // split('-').join('') === Remove hyphens
+console.log(`[Node Adress] === ${nodeAddress}`);
+
+// Create an instance of the Express.js server
 const app = express();
-const bitcoin = new Blockchain();
 
-console.log(bitcoin);
+// Create a new instance of the Blockchain class
+const oroCoin = new Blockchain();
 
+// Configure the Express.js server to parse JSON and URL encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Get the entire blockchain
 app.get('/blockchain', (req, res) => {
-  res.send(bitcoin);
+  res.send({ oroCoin });
 });
 
+// Create a new transaction
 app.post('/transaction', (req, res) => {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
+  let transactionNumber = oroCoin.pendingTransactions.length + 1;
+  const { amount, sender, recipient } = req.body;
+  const blockIndex = oroCoin.createNewTransaction(amount, sender, recipient);
+  res.json({
+    note: `Transaction Number ${transactionNumber} will be added in block ${blockIndex}.`,
+  });
+  transactionNumber++;
 });
 
+// Mine a new block
 app.get('/mine', (req, res) => {
-  res.send('Mine a new block');
+  const lastBlock = oroCoin.getLastBlock();
+  const previousBlockHash = lastBlock['hash'];
+  const currentBlockData = [
+    {
+      transactions: oroCoin.pendingTransactions,
+      index: lastBlock['index'] + 1,
+    },
+  ];
+  const nonce = oroCoin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = oroCoin.hashBlock(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+  );
+
+  oroCoin.createNewTransaction(12.5, '00', nodeAddress);
+
+  const newBlock = oroCoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+  res.json({
+    note: 'New block mined successfully',
+    block: newBlock,
+  });
 });
 
 app.listen(3000, () => {
