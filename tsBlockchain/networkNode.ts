49cbb22523697c576fdabc38ff2f16e3be6aeddb
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-04-04 13:29:02 -0400

    Refactor transaction number in networkNode.ts.

--- tsBlockchain/networkNode.ts
+++ tsBlockchain/networkNode.ts
@@ -23,13 +23,11 @@ app.get('/blockchain', (req, res) => {
 
 // Create a new transaction
 app.post('/transaction', (req, res) => {
-  let transactionNumber = oroCoin.pendingTransactions.length + 1;
   const { amount, sender, recipient } = req.body;
   const blockIndex = oroCoin.createNewTransaction(amount, sender, recipient);
   res.json({
-    note: `Transaction Number ${transactionNumber} will be added in block ${blockIndex}.`,
+    note: `Transaction will be added in block ${blockIndex}.`,
   });
-  transactionNumber++;
 });
 
 // Mine a new block
@@ -59,8 +57,61 @@ app.get('/mine', (req, res) => {
   });
 });
 
-const port = process.argv[2];
+// Register a new node and broadcast it to the network
+app.post('/register-and-broadcast-node', async (req, res) => {
+  const { newNodeUrl } = req.body;
+  if (!oroCoin.networkNodes.includes(newNodeUrl)) {
+    oroCoin.networkNodes.push(newNodeUrl);
+  }
 
+  //   Register the new node with the network
+  try {
+    const regNodesPromises = oroCoin.networkNodes.map(
+      async (networkNodeUrl) => {
+        const response = await fetch(networkNodeUrl + '/register-node', {
+          method: 'POST',
+          body: JSON.stringify({ newNodeUrl }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        return response.json();
+      }
+    );
+
+    await Promise.all(regNodesPromises).then((data) => {
+      const allNetworkNodes = [...data, oroCoin.currentNodeUrl];
+      const bulkRegisterOptions = {
+        method: 'POST',
+        body: JSON.stringify({ allNetworkNodes }),
+        headers: { 'Content-Type': 'application/json' },
+      };
+      return fetch(
+        oroCoin.currentNodeUrl + '/register-nodes-bulk',
+        bulkRegisterOptions
+      );
+    });
+    res.json({ note: 'New node registered with network successfully.' });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ note: 'Error registering new node with network.', error });
+  }
+});
+
+// Register a node with the network
+app.post('/register-node', (req, res) => {
+  const { newNodeUrl } = req.body;
+  const nodeNotAlreadyPresent = !oroCoin.networkNodes.includes(newNodeUrl);
+  const notCurrentNode = oroCoin.currentNodeUrl !== newNodeUrl;
+  if (nodeNotAlreadyPresent && notCurrentNode) {
+    oroCoin.networkNodes.push(newNodeUrl);
+  }
+  res.json({ note: 'New node registered successfully.' });
+});
+
+app.post('/register-nodes-bulk', (req, res) => {});
+
+// Start the Express.js server
+const port = process.argv[2];
 app.listen(port, () => {
   console.log(`Blockchain App Listening on port ${port}...`);
 });
