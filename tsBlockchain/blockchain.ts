a4deb419c95f6dba449e4d0788e7dd1d49f12378
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-04-10 01:34:46 -0400

    Refactor chainIsValid method in Blockchain class.

--- tsBlockchain/blockchain.ts
+++ tsBlockchain/blockchain.ts
@@ -93,6 +93,94 @@ class Blockchain {
     return nonce;
   }
 
+  chainIsValid(blockchain: Block[]): boolean {
+    let validChain = true;
+
+    for (let i = 1; i < blockchain.length; i++) {
+      const currentBlock = blockchain[i];
+      const prevBlock = blockchain[i - 1];
+      const blockHash = this.hashBlock(
+        prevBlock.hash,
+        currentBlock.transactions,
+        currentBlock.nonce
+      );
+      if (blockHash.substring(0, 4) !== '0000') {
+        validChain = false;
+        break;
+      }
+      if (currentBlock.previousBlockHash !== prevBlock.hash) {
+        validChain = false;
+        break;
+      }
+    }
+
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock['nonce'] === 100;
+    const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
+    const correctHash = genesisBlock['hash'] === '0';
+    const correctTransactions = genesisBlock['transactions'].length === 0;
+
+    if (
+      !correctNonce ||
+      !correctPreviousBlockHash ||
+      !correctHash ||
+      !correctTransactions
+    ) {
+      validChain = false;
+    }
+
+    return validChain;
+  }
+
+  // chainIsValid(blockchain: Block[]): boolean {
+  //   let validChain = true;
+
+  //   // Validate Genesis Block separately
+  //   const genesisBlock = blockchain[0];
+  //   const correctNonce = genesisBlock.nonce === 100; // Assuming your genesis block's nonce is 100
+  //   const correctPreviousBlockHash = genesisBlock.previousBlockHash === '0'; // Assuming your genesis block's previous hash is '0'
+  //   const correctHash = genesisBlock.hash.substring(0, 4) === '0000'; // If your condition for a valid hash is that it starts with '0000'
+  //   const correctTransactions = genesisBlock.transactions.length === 0; // If your genesis block should have no transactions
+
+  //   if (
+  //     !correctNonce ||
+  //     !correctPreviousBlockHash ||
+  //     !correctHash ||
+  //     !correctTransactions
+  //   ) {
+  //     return false; // If genesis block is invalid, no need to check the rest
+  //   }
+
+  //   // Validate the rest of the blockchain
+  //   for (let i = 1; i < blockchain.length; i++) {
+  //     const currentBlock = blockchain[i];
+  //     const prevBlock = blockchain[i - 1];
+
+  //     // Validate block hash
+  //     const blockHash = this.hashBlock(
+  //       prevBlock.hash,
+  //       currentBlock.transactions,
+  //       currentBlock.nonce
+  //     );
+  //     if (blockHash.substring(0, 4) !== '0000') {
+  //       validChain = false;
+  //       break; // Exit loop early if any block is invalid
+  //     }
+
+  //     // Validate previous block hash link
+  //     if (currentBlock.previousBlockHash !== prevBlock.hash) {
+  //       validChain = false;
+  //       break; // Exit loop early if any block is invalid
+  //     }
+  //   }
+
+  //   // After full iteration, return the result
+  //   return validChain;
+  // }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
   // Hash Block Using External Crypto-JS NPM Module
   // hashBlock(
   //   previousBlockHash: string,
@@ -107,9 +195,6 @@ class Blockchain {
   // }
 
   // Get Last Block
-  getLastBlock() {
-    return this.chain[this.chain.length - 1];
-  }
 }
 
 export default Blockchain;
